4517ca29c0595b92e4c6f5072ddd53d0c53ba7be
Takaya-Yamazato <yamazato@m.ieice.org>
2020-01-02 14:02:42 +0900

    gitignore を追加

--- index.php
+++ index.php
@@ -4,7 +4,9 @@
 
 <?php
 
-$conn = "host=localhost dbname=ocwpdb-u user=ocwuser password=passwd";
+require_once('./lib/ocw_init.php');
+
+$conn = "host=localhost dbname=ocwdb-u user=ocwuser password=passwd";
 $link = pg_connect($conn);
 if (!$link) {
     die('接続失敗です。'.pg_last_error());
@@ -12,13 +14,35 @@ if (!$link) {
 
 print('接続に成功しました。<br>');
 
-// PostgreSQLに対する処理
+// SQL文の作成
+$sql = "SELECT contents.contents, course.course_id 
+        FROM course 
+        INNER JOIN pages ON course.course_id = pages.course_id 
+        INNER JOIN page_contents ON pages.page_id = page_contents.page_id 
+        INNER JOIN contents ON page_contents.contents_id = contents.pid 
+        INNER JOIN page_type_master ON page_type_master.page_type_code = pages.page_type 
+        WHERE course.course_id = '41' " ;
+print($sql) ;
+echo "<br><br>";
 
+$result = pg_query($sql);
+    if (!$result) {
+        die('クエリーが失敗しました。'.pg_last_error());
+    }
+        
+for ($i = 0 ; $i < pg_num_rows($result) ; $i++){
+    $rows = pg_fetch_array($result, NULL, PGSQL_ASSOC);
+    print_r($rows);
+    echo "<br><br>";
+    print('course.course_id='.$rows[0]);
+    print(',contents.contents='.$rows[1].'<br>');
+        }
+        
 $close_flag = pg_close($link);
-
+        
 if ($close_flag){
     print('切断に成功しました。<br>');
-}
+    }
 
 ?>
 </body>
